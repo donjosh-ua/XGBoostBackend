9dd8e40d73bd961fe419a50b76ddd2a21b5da619
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2025-02-10 22:51:05 -0500

    Updated host

--- app/main.py
+++ app/main.py
@@ -36,4 +36,4 @@ def read_root():
     return {"message": "Bienvenido a la API de XGBoost con FastAPI"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
